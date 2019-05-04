4daa6a371a841e7aa219462fc852664359276f46
Md Saimun Hossain <saimonanam@gmail.com>
2019-05-04 21:16:34 +0600

    dynamic cart appear when select item & close while click

--- app.js
+++ app.js
@@ -120,6 +120,20 @@ class UI{
         cartOverlay.classList.add('transparentBcg');
         cartDOM.classList.add('showCart');
     }
+    setupAPP(){
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click',this.hideCart);
+    }
+    populateCart(cart){
+        cart.forEach(item => this.addCartItem(item));
+    }
+    hideCart(){
+        cartOverlay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    }
 }
 //local storage
 class Storage{
@@ -133,12 +147,16 @@ class Storage{
     static saveCart(cart){
         localStorage.setItem('cart', JSON.stringify(cart));
     }
+    static getCart(){
+        return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')): []
+    }
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const prodcuts = new Products();
-
+    //setup app
+    ui.setupAPP();
     // get all products
     prodcuts.getProducts().then(products => {
     ui.displayProducts(products);

02c57b108e2093939b582fa144c1788371d8fb21
Md Saimun Hossain <saimonanam@gmail.com>
2019-05-04 20:18:54 +0600

    as soon as added product to the cart, cart is appear & showed prodcut on it

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const productsDOM = document.querySelector('.products-center');
 
 //cart
 let cart = [];
+//buttons
+let buttonsDOM = [];
 
 //getting the products
 class Products{
@@ -60,25 +62,76 @@ class UI{
     }
     getBagButtons(){
         const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
         buttons.forEach(button => {
             let id = button.dataset.id;
             let inCart = cart.find(item => item.id === id);
             if(inCart){
                 button.innerText = "In Cart";
                 button.disabled = true
-            }else{
-                button.addEventListener('click', (event)=>{
-                    event.target.innerText = "In Cart";
-                    event.target.disabled = true;
-                })
             }
+            button.addEventListener('click', (event)=>{
+                event.target.innerText = "In Cart";
+                event.target.disabled = true;
+                // get product from products
+                let cartItem = {...Storage.getProduct(id), amount:1};
+                // add product to th ecart
+                cart = [...cart, cartItem];
+                console.log(cart);
+                //save cart in local storage
+                Storage.saveCart(cart)
+                //set cart values
+                this.setCartValues(cart);
+                //display cart item
+                this.addCartItem(cartItem);
+                // show the cart
+                this.showCart();
+            });
         });
+    }
+    setCartValues(cart){
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item =>{
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+    addCartItem(item){
+        const div = document.createElement('div');
+        div.classList.add('cart-item');
+        div.innerHTML = `<img src=${item.image} alt="product">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+        </div>
+        <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+        </div>`;
+        cartContent.appendChild(div);
+        
+    }
+    showCart(){
+        cartOverlay.classList.add('transparentBcg');
+        cartDOM.classList.add('showCart');
     }
 }
 //local storage
 class Storage{
     static saveProdcuts(products){
         localStorage.setItem("products", JSON.stringify(products));
+    }
+    static getProduct(id){
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id)
+    }
+    static saveCart(cart){
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 }
 

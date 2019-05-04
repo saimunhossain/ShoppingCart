2b2460c25ee082cb2cd1bd9ad23a02b24fb3a1ce
Md Saimun Hossain <saimonanam@gmail.com>
2019-05-04 17:37:35 +0600

    showing all products form a static json file

--- app.js
+++ app.js
@@ -16,12 +16,48 @@ let cart = [];
 
 //getting the products
 class Products{
+    async getProducts(){
+        try{
+            let result = await fetch("products.json");
+            let data = await result.json();
 
+            let products = data.items;
+            products = products.map(item => {
+                const {title,price} = item.fields;
+                const {id} = item.sys
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+            })
+            return products
+        }catch(error){
+            console.log(error);
+        }
+    }
 }
 
 // display products
 class UI{
-
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result +=`
+            <!-- single product -->
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to cart
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+        <!-- end of single product -->
+            `
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 //local storage
 class Storage{
@@ -31,4 +67,8 @@ class Storage{
 document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const prodcuts = new Products();
-})
+
+    // get all products
+    prodcuts.getProducts().then(products => 
+    ui.displayProducts(products));
+});

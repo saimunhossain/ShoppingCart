b52f407869935547537a9a1a56658834f2f64b3a
Md Saimun Hossain <saimonanam@gmail.com>
2019-05-05 00:35:33 +0600

    increase item quantity along with total item count

--- app.js
+++ app.js
@@ -147,6 +147,15 @@ class UI{
                 cartContent.removeChild(removeItem.parentElement.parentElement);
                 this.removeItem(id);
             }
+            else if(event.target.classList.contains('fa-chevron-up')){
+                let addAmount = event.target;
+                let id = addAmount.dataset.id;
+                let tempItem = cart.find(item => item.id===id);
+                tempItem.amount = tempItem.amount + 1;
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                addAmount.nextElementSibling.innerText = tempItem.amount;  
+            }
         })
     }
     clearCart(){
